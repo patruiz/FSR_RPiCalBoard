c827072c7e8fd795d4964e707865cd68a2fa884a
patruiz <patrick.ruiz@appliedmedical.com>
2024-04-16 11:07:36 -0700

    swag program!!!!!!!!!!!

--- src/sensors/keysight/keysight.py
+++ src/sensors/keysight/keysight.py
@@ -1,19 +1,26 @@
 import keysight_kt34400 
 import keysight_kt34400.keysight_kt34400
+import pandas as pd 
 import numpy as np 
 import keyboard
 import datetime
 import time
-import csv
 import os 
 
 def store_data(data, fsr_num, save_data = True):
     if save_data == True:
         save_dir = os.path.join('data', 'raw', 'keysight')
-        datetime_stamp = datetime.now().strftime('%d%b%y_%H-%M-%S')
+        datetime_stamp = datetime.datetime.now().strftime('%d%b%y_%H-%M-%S')
         file_name = f"FSR{fsr_num}_calibration_{datetime_stamp}.csv"
-        file_path = os.path.join(save_dir, file_name)
-        print(file_path)
+        file_path = os.path.join(os.getcwd(), save_dir, file_name)
+
+        print(f"Data Saved in: {file_path}")
+        print(f"Data Array Length: {len(data)}")
+
+        data_array = np.array(data)
+
+        df = pd.DataFrame(data_array)
+        df.to_csv(file_path, index = False)
 
 def main(resource_name):
     data = []
@@ -47,19 +54,21 @@ def main(resource_name):
 
         sample_delay = datetime.timedelta(milliseconds=2e+6)
 
-        try:
-            while True:
-                val = driver.measurement.read(sample_delay)
-                print(val)
-                data.append(val)
-        except KeyboardInterrupt:
-            store_data(data, '1', True)
+        flag = True
+        while flag:
+            val = driver.measurement.read(sample_delay)
+            print(val)
+            data.append(val)
+            print(val)
+            if keyboard.is_pressed('space'):
+                flag = False
+        store_data(data, '1', True)
 
     except Exception as e:
         print("\n Exception:", e.__class__.__name__, e.args)
-
+ 
 
 if __name__ == '__main__':
     resource_name = 'USB0::0x2A8D::0x8E01::CN62180061::0::INSTR'
 
-    main(resource_name)
+    main(resource_name) 
